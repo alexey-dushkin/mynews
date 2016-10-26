b773e03b29e616f11aac1c1ede3a78aad9517091
alexey-dushkin <alexey.dushkin@mail.ru>
2016-10-26 19:59:50 +0300

    format code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,159 +1,157 @@
-(function() {
+(function () {
 
 	'use strict';
 
-	angular.module('infinite-scroll').value('THROTTLE_MILLISECONDS', 2000)             
-   	
-	var app = angular.module('mynews', ['ui.router','infinite-scroll']);
-	
-	app.config(function($stateProvider, $urlRouterProvider) {
+	angular.module('infinite-scroll').value('THROTTLE_MILLISECONDS', 2000)
 
-    			$stateProvider									
-    			.state('news', {
-			      url: '/',
-			      controller: 'NewsController',	 
-			      templateUrl: 'news.html'
- 			})
-    			.state('sources', {
-			      url: '/sources',
-			      controller: 'SourcesController',
-			      templateUrl: 'sources.html'
- 			});
-    			$urlRouterProvider.otherwise('/');                           						
-			
-	}); 
+	var app = angular.module('mynews', ['ui.router', 'infinite-scroll']);
+
+	app.config(function ($stateProvider, $urlRouterProvider) {
+
+		$stateProvider
+			.state('news', {
+				url: '/',
+				controller: 'NewsController',
+				templateUrl: 'news.html'
+			})
+			.state('sources', {
+				url: '/sources',
+				controller: 'SourcesController',
+				templateUrl: 'sources.html'
+			});
+		$urlRouterProvider.otherwise('/');
+
+	});
 
 
-	
+
+
 	// Служба загрузки источников
-	app.factory('SourcesService', function($q) {
+	app.factory('SourcesService', function ($q) {
 
-	   return {
-	     sources: [],
-	     
-	     getSources: function(){
-            	var deferred = $q.defer();
-		JsAPI.Dao.getProperties({
-			  	  block_id: 84683
-				  }, function(blockProps) {				        
+		return {
+			sources: [],
+
+			getSources: function () {
+				var deferred = $q.defer();
+				JsAPI.Dao.getProperties({
+					block_id: 84683
+				}, function (blockProps) {
 					deferred.resolve(blockProps.publishers);
-				  }, function(reason) {
-				  	deferred.reject(reason);					
-				  });
-            	
-               return deferred.promise; 	
-             }             
-            
-	  }	
-			     
+				}, function (reason) {
+					deferred.reject(reason);
+				});
+
+				return deferred.promise;
+			}
+
+		}
+
 	});
 
 	// Служба загрузки новостей
-	app.factory('NewsService', function($q) {
+	app.factory('NewsService', function ($q) {
 
-	   return {    	     
-	     getNews: function(cnt,publishers,articleIds){
-            	var deferred = $q.defer();
-		JsAPI.Dao.getArticles({
-				block_id: 84683,
-				count: cnt,
-				fields: 0xFFFF,
-				filter_publishers: publishers,
-				filter_articles: articleIds
-				}, function(articles) {					
-					deferred.resolve(articles);					
-				}, function(reason) {
-					deferred.reject(reason);					
-				});                  		
+		return {
+			getNews: function (cnt, publishers, articleIds) {
+				var deferred = $q.defer();
+				JsAPI.Dao.getArticles({
+					block_id: 84683,
+					count: cnt,
+					fields: 0xFFFF,
+					filter_publishers: publishers,
+					filter_articles: articleIds
+				}, function (articles) {
+					deferred.resolve(articles);
+				}, function (reason) {
+					deferred.reject(reason);
+				});
 
-            	
-               return deferred.promise; 	
-             }             
-            
-	  }	
-			     
+
+				return deferred.promise;
+			}
+
+		}
+
 	});
 
-        
 
-	function MainController($scope)
-	{
-	    $scope.hello = 'Новостной агрегатор';	   	
+
+	function MainController($scope) {
+		$scope.hello = 'Новостной агрегатор';
 	}
 
-	app.controller('MainController', MainController);			
+	app.controller('MainController', MainController);
 
 
 	// NewsController
-	app.controller('NewsController', function ($scope,NewsService,SourcesService) {
-	  		
-	  $scope.articles = [];
+	app.controller('NewsController', function ($scope, NewsService, SourcesService) {
 
-	  // загрузка статей
-	  $scope.loadArticles = function(cnt)  { 
-		
-		 if ($('#news').attr('infinite-scroll-disabled') == 'true') return;
-		 $('#news').attr('infinite-scroll-disabled','true');			 
-	
-		 var publishers = [];
-	 	 var articleIds = [];
+		$scope.articles = [];
 
-		 // Заполняем список загруженных статей
-	 	 $.each($scope.articles, function(){                    
-		    articleIds = articleIds.concat(this.id);		
-    		 });
-		
-		 // Заполняем массив отключенных источников
-	 	 $.each(SourcesService.sources, function(){                    
-		    if (this.show == false)  publishers = publishers.concat(this.id);		
-    		 });
+		// загрузка статей
+		$scope.loadArticles = function (cnt) {
+
+			if ($('#news').attr('infinite-scroll-disabled') == 'true') return;
+			$('#news').attr('infinite-scroll-disabled', 'true');
+
+			var publishers = [];
+			var articleIds = [];
+
+			// Заполняем список загруженных статей
+			$.each($scope.articles, function () {
+				articleIds = articleIds.concat(this.id);
+			});
+
+			// Заполняем массив отключенных источников
+			$.each(SourcesService.sources, function () {
+				if (this.show == false) publishers = publishers.concat(this.id);
+			});
 
 
-		var promiseObj=NewsService.getNews(cnt,publishers,articleIds);
-	        promiseObj.then(function(articles) { 			
-                      	$scope.articles = $scope.articles.concat(articles);							
-			$('#news').attr('infinite-scroll-disabled','false');	
-		});		 
+			var promiseObj = NewsService.getNews(cnt, publishers, articleIds);
+			promiseObj.then(function (articles) {
+				$scope.articles = $scope.articles.concat(articles);
+				$('#news').attr('infinite-scroll-disabled', 'false');
+			});
 
-	   };	
+		};
 
-           
-	   // событие прокрутки    	   
-	   /*
-      	   $(window).scroll(function() {
-        	if($(window).scrollTop()+$(window).height()==($(document).height())){ 		 
-		// если дошли до конца страницы - подгружаем новые статьи
-		$scope.loadArticles(30);   		 
-	     } 
-      	   });
-	   */
-	   
-	   // Начальная загрузка статей
-	   $scope.loadArticles(60); 	
-	  
 
-	});	
+		// событие прокрутки    	   
+		/*
+				$(window).scroll(function() {
+			 if($(window).scrollTop()+$(window).height()==($(document).height())){ 		 
+		 // если дошли до конца страницы - подгружаем новые статьи
+		 $scope.loadArticles(30);   		 
+		  } 
+				});
+		*/
+
+		// Начальная загрузка статей
+		$scope.loadArticles(60);
+
+
+	});
 
 	// SourcesController
-	app.controller('SourcesController', function ($scope,SourcesService) {	   
-	        if (SourcesService.sources.length == 0)
-		{
-			var promiseObj=SourcesService.getSources();
-	        	promiseObj.then(function(sources) { 
-				SourcesService.sources=sources; 
+	app.controller('SourcesController', function ($scope, SourcesService) {
+		if (SourcesService.sources.length == 0) {
+			var promiseObj = SourcesService.getSources();
+			promiseObj.then(function (sources) {
+				SourcesService.sources = sources;
 				$scope.sources = SourcesService.sources;
 				// Изначально все источники включены
-				$.each(SourcesService.sources, function(){                    			
-		    			this.show = true;		
-    		 		});
+				$.each(SourcesService.sources, function () {
+					this.show = true;
+				});
 			});
 		}
-		else
-		{
+		else {
 			$scope.sources = SourcesService.sources;
 		}
-		
-	});	
+
+	});
 
 
 
